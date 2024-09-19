e7884768cefc287b0642101d321ead090f6683ae
Justin Stevens <js2024@iastate.edu>
2024-09-19 11:17:58 -0500

    added step count display to daily and weekly

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/Goals.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/Goals.java
@@ -10,14 +10,21 @@ import android.widget.TextView;
 
 public class Goals extends AppCompatActivity {
 
-    private TextView messageText;
+    private TextView daily_step_disp;
+    private TextView weekly_step_disp;
+    private int dailyStepCount = 1000;
+    private int weeklyStepCount = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.goals);
 
+        daily_step_disp = findViewById(R.id.dailySteps);
+        weekly_step_disp = findViewById(R.id.weeklySteps);
 
+        daily_step_disp.setText(dailyStepCount + "/10000");
+        weekly_step_disp.setText(weeklyStepCount + "/70000");
 
 
     }
