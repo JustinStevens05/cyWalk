7fcf1219ccb9fc77cc43155b9ce8fe93f3e7f68c
Justin Stevens <js2024@iastate.edu>
2024-09-20 08:15:25 -0500

    added progress bar progression and section titles

--- Frontend/AndroidExample/app/src/main/java/com/example/androidexample/Goals.java
+++ Frontend/AndroidExample/app/src/main/java/com/example/androidexample/Goals.java
@@ -6,14 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class Goals extends AppCompatActivity {
 
     private TextView daily_step_disp;
     private TextView weekly_step_disp;
+    private ProgressBar daily_bar;
+    private ProgressBar weekly_bar;
     private int dailyStepCount = 1000;
     private int weeklyStepCount = 5000;
+    private final int dailyGoal = 10000;
+    private final int weeklyGoal = 70000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +27,14 @@ public class Goals extends AppCompatActivity {
 
         daily_step_disp = findViewById(R.id.dailySteps);
         weekly_step_disp = findViewById(R.id.weeklySteps);
+        daily_bar = findViewById(R.id.dailyprogressBar);
+        weekly_bar = findViewById(R.id.weeklyprogressBar);
 
-        daily_step_disp.setText(dailyStepCount + "/10000");
-        weekly_step_disp.setText(weeklyStepCount + "/70000");
+        daily_step_disp.setText(dailyStepCount + "/" + dailyGoal);
+        weekly_step_disp.setText(weeklyStepCount + "/" + weeklyGoal);
 
+        daily_bar.setProgress(dailyStepCount);
+        weekly_bar.setProgress(weeklyStepCount);
 
     }
 }
