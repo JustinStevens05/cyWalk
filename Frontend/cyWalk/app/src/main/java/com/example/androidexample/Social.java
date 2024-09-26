0b4fb3ac55e197cad09ff33a3a8445c4604177d2
Justin Stevens <js2024@iastate.edu>
2024-09-26 17:41:13 -0500

    created social page skeleton

---
+++ Frontend/cyWalk/app/src/main/java/com/example/androidexample/Social.java
@@ -0,0 +1,19 @@
+package com.example.androidexample;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Social extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.social);
+
+
+
+    }
+}
