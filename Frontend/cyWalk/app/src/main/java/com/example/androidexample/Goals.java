3ef744dcb9cea24a22deb6d2b92238220d46cac7
Justin Stevens <js2024@iastate.edu>
2024-10-01 14:42:12 -0500

    used volley and postman to get user data from a database

--- Frontend/cyWalk/app/src/main/java/com/example/androidexample/Goals.java
+++ Frontend/cyWalk/app/src/main/java/com/example/androidexample/Goals.java
@@ -4,10 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Goals extends AppCompatActivity {
 
@@ -15,11 +29,13 @@ public class Goals extends AppCompatActivity {
     private TextView weekly_step_disp;
     private ProgressBar daily_bar;
     private ProgressBar weekly_bar;
-    private int dailyStepCount = 1000;
-    private int weeklyStepCount = 51000;
+    private int dailyStepCount = 0;
+    private int weeklyStepCount = 0;
     private final int dailyGoal = 10000;
     private final int weeklyGoal = 70000;
     private Button socialButton;
+
+    private static final String URL_JSON_OBJECT = "https://a7d1bdb7-5276-4165-951c-f32dee760766.mock.pstmn.io/users?userId=3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +48,7 @@ public class Goals extends AppCompatActivity {
         daily_bar = findViewById(R.id.dailyprogressBar);
         weekly_bar = findViewById(R.id.weeklyprogressBar);
 
-        daily_step_disp.setText(dailyStepCount + "/" + dailyGoal);
-        weekly_step_disp.setText(weeklyStepCount + "/" + weeklyGoal);
-
-        daily_bar.setProgress(dailyStepCount);
-        weekly_bar.setProgress(weeklyStepCount);
+        makeJsonObjReq();
 
         socialButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,5 +59,61 @@ public class Goals extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    private void makeJsonObjReq() {
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                Request.Method.GET,
+                URL_JSON_OBJECT,
+                null, // Pass null as the request body since it's a GET request
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Volley Response", response.toString());
+                        try {
+                            // Parse JSON object data
+                            String dsteps = response.getString("dailySteps");
+                            String wsteps = response.getString("weeklySteps");
+
+                            dailyStepCount = Integer.parseInt(dsteps);
+                            weeklyStepCount = Integer.parseInt(wsteps);
+
+                            // Populate text views with the parsed data
+                            daily_step_disp.setText(dailyStepCount + "/" + dailyGoal);
+                            weekly_step_disp.setText(weeklyStepCount + "/" + weeklyGoal);
+
+                            daily_bar.setProgress(dailyStepCount);
+                            weekly_bar.setProgress(weeklyStepCount);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley Error", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+//                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+//                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+//                params.put("param1", "value1");
+//                params.put("param2", "value2");
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjReq);
     }
 }
