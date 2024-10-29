46167bee257053f7c6ca741870ff1fff4c8d6a66
Charlie Dolphin <charlie.dolphin9@gmail.com>
2024-10-29 15:21:26 -0500

    profile gets username properly via api

--- Frontend/cyWalk/app/src/main/java/com/example/androidexample/Profile.java
+++ Frontend/cyWalk/app/src/main/java/com/example/androidexample/Profile.java
@@ -2,17 +2,31 @@ package com.example.androidexample;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Profile extends AppCompatActivity {
 
     private static String key;
-    //TextView txt_response;
+    private static String URL_JSON_OBJECT = null;
+    private String username;
+    TextView txt_username;
 
 
     @Override
@@ -23,6 +37,8 @@ public class Profile extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         key = extras.getString("key");
         //txt_response.setText("Key: " + key);
+        URL_JSON_OBJECT = "http://10.0.2.2:8080/users/"+key;
+        txt_username = findViewById(R.id.profile_txt_username);
 
         // NAVIGATION BAR
         BottomNavigationView botnav = findViewById(R.id.bottomNavigation);
@@ -61,5 +77,50 @@ public class Profile extends AppCompatActivity {
                 return false;
             }
         });
+        makeJsonObjReq();
+    }
+
+    private void makeJsonObjReq() {
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                Request.Method.GET, URL_JSON_OBJECT, null, // Pass null as the request body since it's a GET request
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Volley Response", response.toString());
+                        try {
+                            // Parse JSON object data
+                            username = response.getString("username");
+                            txt_username.setText(username);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley Error", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+//                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+//                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+//                params.put("param1", "value1");
+//                params.put("param2", "value2");
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjReq);
     }
 }
