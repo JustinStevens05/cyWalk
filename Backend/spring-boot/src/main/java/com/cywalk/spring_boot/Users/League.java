c1042f2fdbe31249e598248fcedd8f5d5d0eaece
ckugel <calebkugel1@gmail.com>
2024-11-17 18:06:01 -0600

    skeleton and endpoints for league and rankings are done

---
+++ Backend/spring-boot/src/main/java/com/cywalk/spring_boot/Users/League.java
@@ -0,0 +1,8 @@
+package com.cywalk.spring_boot.Leaderboard;
+
+/**
+ * An eum representing the league a user could be in. The ordinal value has (higher better)
+ */
+public enum League {
+    COPPER, BRONZE, SILVER, GOLD, PLATINUM, DIAMOND, CY
+}
