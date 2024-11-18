a92ff433bb3edc9df9615a5585dcd1c43f869514
Caleb Kugel <calebkugel1@gmail.com>
2024-11-18 12:54:24 -0600

    entitys and models documented with swagger

--- Backend/spring-boot/src/main/java/com/cywalk/spring_boot/Users/UserModel.java
+++ Backend/spring-boot/src/main/java/com/cywalk/spring_boot/Users/UserModel.java
@@ -1,24 +1,29 @@
 package com.cywalk.spring_boot.Users;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import org.springframework.lang.NonNull;
 
 @Entity
+@Schema(description = "A model that contains a secret key for a user")
 public class UserModel {
 
     @Id
     @GeneratedValue
+    @Schema(description = "the secret key to be used in endpoints for a user")
     private long secretKey;
 
     @ManyToOne
+    @Schema(description = "the user who matches with the secret key")
+    @NonNull
     private People people;
 
-
-    public UserModel(long secretKey, People people) {
+    public UserModel(long secretKey, @NonNull People people) {
         this.secretKey = secretKey;
         this.people = people;
     }
 
-    public UserModel(People people) {
+    public UserModel(@NonNull People people) {
         this.people = people;
     }
 
