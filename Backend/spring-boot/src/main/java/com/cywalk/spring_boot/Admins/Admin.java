e7f909116a206da31fadcc8d865255d7906716c0
Caleb Kugel <calebkugel1@gmail.com>
2024-12-07 15:47:45 -0600

    sign up endpoint

--- Backend/spring-boot/src/main/java/com/cywalk/spring_boot/Admins/Admin.java
+++ Backend/spring-boot/src/main/java/com/cywalk/spring_boot/Admins/Admin.java
@@ -1,16 +1,63 @@
 package com.cywalk.spring_boot.Admins;
 
 import com.cywalk.spring_boot.Organizations.Organization;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 
 @Entity
 public class Admin {
     @ManyToOne
     @JoinColumn(name = "organization_id")
+    @Schema(description = "Organization", example = "ISU")
     private Organization organization;
 
-    @Id
+    @Schema(description = "Admin name", example = "John Doe")
+    @Column(unique = false, nullable = false)
     private String name;
 
+    @Schema(description = "Admin combined name", example = "ISU John Doe")
+    @Id
+    @Column(unique = true, nullable = false)
+    private String combinedName;
+
+    public Admin() {}
+
+    public void updateCombinedName() {
+        this.combinedName = organization.getName() + " " + name;
+    }
+
+    public Admin(Organization organization, String name) {
+        this.organization = organization;
+        this.name = name;
+        updateCombinedName();
+    }
+
+    public Admin(Organization organization, String name, String combinedName) {
+        this.organization = organization;
+        this.name = name;
+        this.combinedName = combinedName;
+    }
+
+    public Organization getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(Organization organization) {
+        this.organization = organization;
+        updateCombinedName();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        updateCombinedName();
+    }
+
+    public String getCombinedName() {
+        return organization.getName() + " " + name;
+    }
 
 }
--- Backend/spring-boot/src/main/java/com/cywalk/spring_boot/Admins/AdminRepository.java
+++ Backend/spring-boot/src/main/java/com/cywalk/spring_boot/Admins/AdminRepository.java
@@ -2,5 +2,8 @@ package com.cywalk.spring_boot.Admins;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AdminRepository extends JpaRepository<Admin, String> {
+    List<Admin> findByOrganizationId(Long organizationId);
 }
