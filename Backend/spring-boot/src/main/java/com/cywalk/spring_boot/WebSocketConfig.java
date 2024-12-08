632a021ccbf8e4d07697ddaf706521269ee75d51
Ethan Ashihundu <ethana1@iastate.edu>
2024-12-07 23:41:26 -0600

    Fixed StepGoal GET function

--- Backend/spring-boot/src/main/java/com/cywalk/spring_boot/WebSocketConfig.java
+++ Backend/spring-boot/src/main/java/com/cywalk/spring_boot/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.cywalk.spring_boot;
 
 import com.cywalk.spring_boot.Friends.FriendLocationController;
 import com.cywalk.spring_boot.Locations.LocationSessionController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -11,27 +12,31 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 @Configuration
 public class WebSocketConfig implements WebSocketConfigurer {
 
-
-    private final LocationSessionController locationSessionController;
-    private final FriendLocationController friendLocationController;
+    private LocationSessionController locationSessionController;
+    private FriendLocationController friendLocationController;
 
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
     }
 
-    public WebSocketConfig(FriendLocationController friendLocationController, LocationSessionController locationSessionController) {
+    @Autowired
+    public void setFriendLocationController(FriendLocationController friendLocationController) {
         this.friendLocationController = friendLocationController;
+    }
+
+    @Autowired
+    public void setLocationSessionController(LocationSessionController locationSessionController) {
         this.locationSessionController = locationSessionController;
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(friendLocationController, "/locations/friends")
-                .setAllowedOrigins("*"); // Set allowed origins as needed
+                .setAllowedOrigins("*");
 
         registry.addHandler(locationSessionController, "/locations/sessions")
-                .setAllowedOrigins("*"); // Set allowed origins as needed
+                .setAllowedOrigins("*");
 
     }
 }
