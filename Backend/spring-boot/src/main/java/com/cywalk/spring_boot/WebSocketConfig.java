7c728ecd4f971233c43a9ed747988b49fc9a1c9d
ckugel <calebkugel1@gmail.com>
2024-12-11 03:21:15 -0600

    combined sockets and enabled the correct features

--- Backend/spring-boot/src/main/java/com/cywalk/spring_boot/WebSocketConfig.java
+++ Backend/spring-boot/src/main/java/com/cywalk/spring_boot/WebSocketConfig.java
@@ -5,13 +5,18 @@ import com.cywalk.spring_boot.Locations.LocationSessionController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
+@EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
+    /**
+     * Must autowire the controllers that will be used as web socket handlers.
+     */
     private LocationSessionController locationSessionController;
     private FriendLocationController friendLocationController;
 
@@ -30,6 +35,9 @@ public class WebSocketConfig implements WebSocketConfigurer {
         this.locationSessionController = locationSessionController;
     }
 
+    /**
+     * Whenever you want to add a web socket handler, you need to register it here.
+     */
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(friendLocationController, "/locations/friends")
